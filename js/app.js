9012bcfff59ec9b3d61b66cbedc517cef263f6d5
Matthew T. Renwick <mrenny65@gmail.com>
2022-07-12 20:22:03 -0600

    fixed some js issues

--- js/app.js
+++ js/app.js
@@ -6,19 +6,21 @@ alert(`Welcome to my site ${siteVisitor}! Guess these facts about me.`);
 
 let questionOne = prompt('Yes or No...Was my High Schools mascot an Indian?').toLowerCase();
 
-if(questionOne === 'yes' || questionOne === 'y'){
+if(questionOne === 'y' || questionOne === 'yes'){
   alert('You are correct!');
-} else if(questionOne === 'no' || questionOne === 'n'){
+  //console.log(questionOne)
+} else if(questionOne === 'n' || questionOne === 'no'){
   alert('You are wrong...');
 } else{
   alert('Answer yes or no')
 }
 
-let questionTwo = prompt('Was my first job as a Cook or Deckhand?').toLowerCase();
+let questionTwo = prompt('Was my first job as a Cook?').toLowerCase();
 
-if (questionTwo === 'Cook' || questionTwo === 'cook'){
+if (questionTwo === 'y' || questionTwo === 'yes'){
   alert('Well done! You are good at this!')
-} else if(questionTwo === 'Deckhand' || questionTwo === 'deckhand'){
+  //console.log(questionTwo)
+} else if(questionTwo === 'n' || questionTwo === 'no'){
   alert('That is nice of you,..but Wrong!')
 } else {
   alert('Maybe you just dont care?')
@@ -26,9 +28,10 @@ if (questionTwo === 'Cook' || questionTwo === 'cook'){
 
 let questionThree = prompt('Based on my appearance, do you think my hobbies include going to the gym?').toLowerCase();
 
-if (questionThree === 'yes' || questionThree === 'Yes'){
+if (questionThree === 'y' || questionThree === 'yes'){
   alert('Gee thanks! I try!')
-} else if(questionThree === 'no' || questionThree === 'No'){
+  //console.log(questionThree)
+} else if(questionThree === 'no' || questionThree === 'no'){
   alert('Six-packs are bought at the store...right?')
 } else {
   alert('I will never be Arnold, but that doesnt mean I dont care?')
@@ -36,19 +39,21 @@ if (questionThree === 'yes' || questionThree === 'Yes'){
 
 let questionFour = prompt('Would you believe that Underwater Basket Weaving is a skill I look to add to my repertoire?').toLowerCase();
 
-if (questionFour === 'yes' || questionFour === 'Yes'){
+if (questionFour === 'y' || questionFour === 'yes'){
   alert('Wow, I must be old if you dont get that joke.')
-} else if (questionFour === 'no' || questionFour === 'No'){
+  //console.log(questionFour)
+} else if (questionFour === 'n' || questionFour === 'no'){
   alert ('Good! I would like to think not!')
 } else {
   alert('Ignoring this is actually the proper response. Well done!')
 }
 
-let questionFive = prompt('One of my personal goals is to hike the PCT in full, True or False?').toLowerCase();
+let questionFive = prompt('One of my personal goals is to hike the PCT in full, yes or no?').toLowerCase();
 
-if (questionFive === 'True' || questionFive === 'true' || questionFive === 't'){
+if (questionFive === 'y' || questionFive === 'yes'){
   alert('Yes, that is correct!')
-} else if (questionFive === 'False' || questionFive === 'false' || questionFive === 'f'){
+  //console.log(questionFive)
+} else if (questionFive === 'n' || questionFive === 'no'){
   alert ('It would be dificult, but I think I could get it done')
 } else {
   alert ('Have any suggestions?')
